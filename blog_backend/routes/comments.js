26853e03f3d7098650618e2de92866fa843f190a
Nguyen Dang Manh Cuong <manhcuongnguyen212@gmail.com>
2025-10-15 19:22:05 +0700

    final

--- blog_backend/routes/comments.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getCommentsByPost, addComment, deleteComment } = require("../controllers/commentController");
-
-router.get("/post/:postId", getCommentsByPost);
-router.post("/", addComment);
-router.delete("/:id", deleteComment);
-
-module.exports = router;
