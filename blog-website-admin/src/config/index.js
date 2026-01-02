525327a48bd7f36f954798aabbf0ce15f867d060
DuyNguyen62 <DuyNguyen62@users.noreply.github.com>
2026-01-02 14:27:28 +0700

    Cập nhật cấu hình BASE_URL cho admin

--- blog-website-admin/src/config/index.js
+++ blog-website-admin/src/config/index.js
@@ -1,1 +1,7 @@
-export const BASE_URL = 'http://localhost:5000/api/v1'
+const isProd = process.env.NODE_ENV === "production";
+export const BASE_URL = isProd
+  ? "https://doanlaptrinhweb-server.onrender.com/api/v1"
+  : "http://localhost:5000/api/v1";
+export const BASE_IMAGE_URL = isProd
+  ? "https://doanlaptrinhweb-server.onrender.com"
+  : "http://localhost:5000";
